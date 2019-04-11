93db4a5dbaf1ad2868de7f0079bdc0179de1568c
George Higbie <ghigbie@monigle.com>
2019-04-11 08:51:06 -0600

    Add imports for forms

--- myproject/forms.py
+++ myproject/forms.py
@@ -1,4 +1,4 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, Password, SubmitField, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo
 
