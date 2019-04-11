9cb917126c95b27a62bd295fa0480c891a68c91e
George Higbie <ghigbie@monigle.com>
2019-04-11 09:01:30 -0600

    Add check email method

--- myproject/forms.py
+++ myproject/forms.py
@@ -2,3 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo
 
+class RegistrationFor(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email(), EqualTo('email_confirm', message='Emails must match')])
+    email_confirm = StringField('Confirm Emial', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match.')])
+    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email has already been registered. Please use another email.')
