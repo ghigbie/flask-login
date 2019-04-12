c26b2784f00a2fe69b207463786e643b1c02b39b
ghigbie <gehigbie@gmail.com>
2019-04-11 21:38:35 -0600

    Edit data in register route

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data, 
+        user = User(first_name=form.first_name,
+                    last_name=form.last_name,
+                    email=form.email.data, 
                     username=form.username.data, 
                     password=form.password.data)
         db.session.add(user)
