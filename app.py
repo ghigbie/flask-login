9d97a0fa2151dba9ad62c57d3d96e7b43e82b1d3
ghigbie <gehigbie@gmail.com>
2019-04-11 19:53:46 -0600

    Edit naming

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from myprojecct import app, db 
+from myproject import app, db 
 from flask import render_template, redirect, request, url_for, flash, abort
 from flask_login import login_user,login_required, logout_user
 from myproject.models import User
--- myproject/forms.py
+++ myproject/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo
 from myproject.models import User
 
-class LoginField(FlaskForm):
+class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
