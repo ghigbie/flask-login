e84d0a968bf2f6a1d47aabf6eb51d2e83177d1c3
ghigbie <gehigbie@gmail.com>
2019-04-11 20:38:03 -0600

    Add basics of login route

--- app.py
+++ app.py
@@ -21,3 +21,13 @@ def logout():
     logout_user()
     flash("You are now securely logged out. Thank you for visiting!")
     return redirect(url_for('home'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user.check_password(form.password.data) and user is not none:
+            login_user(user)
+            flash('Logged in Successfully!')
+            next = request.args.get('next')
