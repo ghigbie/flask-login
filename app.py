41e331bd9a003d0e4526d860dd466899ae0da8b3
ghigbie <gehigbie@gmail.com>
2019-04-11 21:07:27 -0600

    Add conditional rendering of links

--- app.py
+++ app.py
@@ -27,11 +27,28 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user.check_password(form.password.data) and user is not none:
+        if user.check_password(form.password.data) and user is not None:
             login_user(user)
             flash('Logged in Successfully!')
             next = request.args.get('next')
             if next == None or not next[0] == '/':
                 next = url_for('welcome_user')
             return redirect(next)
-        return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, 
+                    username=form.username.data, 
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Thanks for registration!")
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
